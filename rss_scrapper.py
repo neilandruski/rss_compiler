455d2da5855a808a553c71b88a9ba1edd3b31197
Neil Andruski <neil.andruski@gmail.com>
2024-02-17 19:50:59 -0500

    verison 0.2.0 working intial version.

--- rss_scrapper.py
+++ rss_scrapper.py
@@ -8,9 +8,10 @@ from rss_parser import Parser
 STYLE = \
     '<meta charset="UTF-8">\n'\
     '<meta name="viewport" content="width=device-width, initial-scale=1.0">\n'\
-    '<link rel="preconnect" href="https://fonts.googleapis.com">'\
-    '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>'\
-    '<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&family=Roboto&display=swap" rel="stylesheet">'\
+    '<link rel="preconnect" href="https://fonts.googleapis.com">\n'\
+    '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>\n'\
+    '<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono" rel="stylesheet">\n'\
+    '<link href="https://fonts.googleapis.com/css2?family=Bevan" rel="stylesheet">\n'\
     '<link rel="stylesheet" href="style.css">\n'\
 
 SITES = [
@@ -18,7 +19,11 @@ SITES = [
     "https://www.bleepingcomputer.com/feed/"
 ]
 
-FLAGS = ['russia']
+FLAGS = ['atlassian',
+         'global protect',
+         'macOS'
+         'windows 11',
+         'microsoft']
 #DATE = datetime.datetime()
 
 ### Class
@@ -53,8 +58,8 @@ class Article:
         tag:str = f'item item-{ number + 1 }'
         if(self.flag): tag +=" flag"
         
-        title = f'<h3><a href="{ self.link }">{ self.title }</a></h3>\n'
-        date = f'<p><b>{ self.date[:16] }</b></p>\n'
+        title = f'<h3><a href="{ self.link }" target="_blank">{ self.title }</a></h3>\n'
+        date = f'<p class="date">{ self.date[:16] }</p>\n'
         body = f'<p>{ self.description }...</p>\n' 
         block = f'<article class="{ tag }">{ title }{ date }{ body }</article>\n'
         return block
@@ -86,11 +91,16 @@ if __name__ =="__main__":
         articles = parse_xml(xml)
         articles_list += articles
     
-    html=f'<!DOCTYPE html><head>{ STYLE }</head>\n<body>\n<div id="bar"><h1>Security Trough</h1></div>\n<div class="container">\n'
+    html=f'<!DOCTYPE html><head>\n{ STYLE }</head>\n<body>\n'\
+        '<div id="bar">\n'\
+        '<div><h1>Security Trough</h1><h6>Come get your feed</h6></div>\n'\
+        '<div id="sheep"> <img src="sheep.png" alt="Sheep"></div>\n'\
+        '</div>\n'\
+        '<div class="container">\n'
     for article in articles_list:
          html += article.get_html(articles_list.index(article))
 
-    html += '</div>\n</body>\n'
+    html += '</div><div id="flock"><img src="sheep_trough.png" alt="Sheep Trough"></div>\n</body>\n'
 
     with open('index.html', "w") as f:
         f.write(html)
